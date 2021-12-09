5b60f529a3ed7f8c28dcc80cec13ec0dc0343513
Muhammad Masduki <muhammad.masduki@student.kit.edu>
2021-12-09 03:47:28 +0100

    Sensor Test 1

--- Drummer.js
+++ Drummer.js
@@ -8,6 +8,10 @@ new Audio('Drums/Hi-hat (open).mp3'),
 new Audio('Drums/Hi-hat (open).mp3')
 ];
 var currSound = 0;
+var cx = 0;
+var cy = 0;
+var dx = [0,0];
+var dy = [0,0];
 /*30 - 70*/
 window.addEventListener("deviceorientation", function(event) {
 	document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma;
@@ -21,7 +25,7 @@ window.addEventListener("deviceorientation", function(event) {
     if (event.beta > 70) {
         loaded = true;
     }
-    if (event.beta < 30) {
+    if (0 < event.beta < 30) {
         if (loaded) {
             loaded = false;
             if(event.beta < 30) {
@@ -38,11 +42,12 @@ window.addEventListener("deviceorientation", function(event) {
 }, true);
 
 window.ondevicemotion = function(event) { 
-	var ax = event.accelerationIncludingGravity.x
-	var ay = event.accelerationIncludingGravity.y
-	var az = event.accelerationIncludingGravity.z
+	var ax = event.acceleration.x
+	var ay = event.acceleration.y
+	var az = event.acceleration.z
 
 	document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az;
+
 }
 
 function playSound() {
