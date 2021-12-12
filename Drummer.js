131252b70e2f95c03d8657db88ae0ce920013eea
Muhammad Masduki <muhammad.masduki@student.kit.edu>
2021-12-12 19:29:34 +0100

    1

--- Drummer.js
+++ Drummer.js
@@ -1,12 +1,12 @@
-var triggered = false;
-var loaded = false;
-var audio;
-var audioT;
-var currSound = 0;
-var path = 0;
+/* determine mode */
 var buttonMuted = true;
+var stickMode = true;
+var footMode = false;
 
-var soundPath = ["Drums/Hi-hat (Closed).mp3",
+/* audio stuff */
+var audio;
+var path;   /* which audio path is currently chosen */
+var audioPath = ["Drums/Hi-hat (Closed).mp3",
     "Drums/Hi-hat (Open).mp3",
     "Drums/Crash Cymbal.mp3",
     "Drums/Ride Cymbal.mp3",
@@ -17,32 +17,11 @@ var soundPath = ["Drums/Hi-hat (Closed).mp3",
     "Drums/Snare Drum (Cross Stick).mp3",
     "Drums/Hi-hat (Foot).mp3",
     "Drums/Bass Drum.mp3"
-];
+];  /* paths to audio files */
 
-window.addEventListener("deviceorientation", function(event) {
-	
-    
-    /*if (event.beta > 70) {
-        loaded = true;
-    }
-    if (event.beta < 30) {
-        if (loaded) {
-            loaded = false;
-            if(event.beta < 30) {
-                if(audioT[currSound].currentTime == 0 || audioT[currSound].ended) {
-                    audioT[currSound].play();
-                }
-                currSound = (currSound + 1) % audioT.length;
-            
-            }
-        }
+var loaded = false; /* device needs to be loaded before firing a sound with motion*/
 
-    }*/
-    
-    document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma + path;
-
-}, true);
-
+/*  motion detection */
 window.ondevicemotion = function(event) { 
 	var ax = event.accelerationIncludingGravity.x
 	var ay = event.accelerationIncludingGravity.y
@@ -50,26 +29,37 @@ window.ondevicemotion = function(event) {
 
 	document.querySelector("#acc").innerHTML = "X = " + ax + "<br>" + "Y = " + ay + "<br>" + "Z = " + az + "<br>";
 
-    if (ax < 1) {
-        loaded = true;
-    }
-    if (ax > 7.5) {
-        if (loaded) {
-            loaded = false;
-                /*if(audioT[currSound].currentTime == 0 || audioT[currSound].ended) {
-                    audioT[currSound].play();
-                }
-                currSound = (currSound + 1) % audioT.length;*/
-            playSound();
-            
-            
+    if (stickMode == true) {
+        if (ax < 1) {   /* raising phone high enough will load the device */
+            loaded = true;
+        }
+        if (ax > 7.5) {
+            if (loaded) {
+                loaded = false;
+                    /*if(audioT[currSound].currentTime == 0 || audioT[currSound].ended) {
+                        audioT[currSound].play();
+                    }
+                    currSound = (currSound + 1) % audioT.length;*/
+                playSound();     
+            }
         }
     }
 
+    if (footMode == true) {
+        if (ax > 1) {
+            loaded = true;
+        }
+        if (ax < -1) {
+            if (loaded) {
+                loaded = false;
+                playSound();           
+            }
+        }
+    }
 }
 
 function playSound() {
-    audio = new Audio(soundPath[path]);
+    audio = new Audio(audioPath[path]);
     audio.play();
 }
 
@@ -80,6 +70,27 @@ function buttonClick(number) {
     }
     document.querySelector("#acc").innerHTML = path;
 }
+
+function muteToggle() {
+    if (buttonMuted == false) {
+        buttonMuted = true;
+    } else {
+        buttonMuted = false;
+    }
+}
+
+function stickModeToggle() {
+    stickMode = true;
+    footMode = false;
+}
+
+function footModeToggle() {
+    stickMode = false;
+    footMode = true;
+}
+
+/*var audioT;
+var currSound = 0;*/
 
 /*function playSound() {
     if(audioT[currSound].currentTime == 0 || audioT[currSound].ended) {
@@ -103,10 +114,26 @@ function soundChange(number) {
     }
 }*/
 
-function changeButtonBehavior() {
-    if (buttonMuted == false) {
-        buttonMuted = true;
-    } else {
-        buttonMuted = false;
+/*window.addEventListener("deviceorientation", function(event) {
+	
+    
+    if (event.beta > 70) {
+        loaded = true;
     }
-}
+    if (event.beta < 30) {
+        if (loaded) {
+            loaded = false;
+            if(event.beta < 30) {
+                if(audioT[currSound].currentTime == 0 || audioT[currSound].ended) {
+                    audioT[currSound].play();
+                }
+                currSound = (currSound + 1) % audioT.length;
+            
+            }
+        }
+
+    }
+    
+    document.querySelector("#mag").innerHTML = "alpha = " + event.alpha + "<br>" + "beta = " + event.beta + "<br>" + "gamma = " + event.gamma + path;
+
+}, true);*/
